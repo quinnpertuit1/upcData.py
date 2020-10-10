6739b38eef7562c9608197e04be43dea1b603283
Quinn Pertuit <qpertuit@gmail.com>
2020-10-09 21:50:29 -0400

    test.py

--- test.py
+++ test.py
@@ -3,23 +3,20 @@
 import json, requests, sys, pprint
 import pandas as pd 
 
-d = []
+d = pd.DataFrame()
 indf = pd.read_csv("in.csv")
 
 a1 = indf["upc"]
 
 for i in a1:
-
    upc = i
-
    url ='https://api.upcitemdb.com/prod/trial/lookup?upc=%s' % (upc)
    response = requests.get(url)
    response.raise_for_status() 
    a = json.loads(response.text)
    b = a["items"]
    c = pd.DataFrame(b)
-   d = d.append(c)
+   d = pd.concat([d,c],axis=0, ignore_index=True)
    
-e = pd.concat(d)
    
-e.to_csv("upc.csv")
+d.to_csv("upc.csv")
